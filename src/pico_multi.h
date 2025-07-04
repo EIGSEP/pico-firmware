2a70b884a766499ba4d1f2bd55963b848cd6f77c
Christian Hellum Bye <chbye@berkeley.edu>
2025-07-04 12:02:07 -0700

    back to default app order

--- src/pico_multi.h
+++ src/pico_multi.h
@@ -12,10 +12,10 @@
 #define LED_PIN PICO_DEFAULT_LED_PIN
 
 // Mapping of DIP switches to APPs
-#define APP_MOTOR       3
+#define APP_MOTOR       0
 #define APP_TEMPCTRL    1
 #define APP_TEMPMON     2
-#define APP_IMU         0
+#define APP_IMU         3
 #define APP_LIDAR       4
 #define APP_RFSWITCH    5
 
