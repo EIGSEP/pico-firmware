aa9b4a1def69f516c838523f07c07d570ccd43f4
Aaron Parsons <aparsons@berkeley.edu>
2025-07-07 16:02:46 -0700

    Updated rfswtich test code to use pichost PicoRFSwitch

--- picohost/scripts/test_rfswitch_pico.py
+++ picohost/scripts/test_rfswitch_pico.py
@@ -1,65 +1,30 @@
 #!/usr/bin/env python3
 import sys
-import json
-from serial import Serial
-import threading
+import picohost
+import time
 
-
-class RFSwitchSerial(Serial):
-    """ """
-
-    def __init__(self, port, baud=115200, timeout=1.0):
-        Serial.__init__(self, port, baud, timeout=timeout)
-        self._running = True
-        self._process_status_thread = None
-
-    def process_status(self):
-        while self._running:
-            line = self.readline().decode("utf-8", errors="ignore").strip()
-            if len(line) == 0:
-                continue
-            status = json.loads(line)
-            print(status)
-
-    def command(self, val_dict):
-        json_str = json.dumps(val_dict, separators=(",", ":")).encode("utf-8")
-        self.write(json_str + b"\n")  # send raw bytes
-        self.flush()
-
-    def start(self):
-        self._running = True
-        self._process_status_thread = threading.Thread(
-            target=self.process_status, daemon=True
-        )
-        self._process_status_thread.start()
-
-    def stop(self):
-        self._running = False
-        if self._process_status_thread is not None:
-            self._process_status_thread.join()
-
+STATES = [
+    'VNAO',
+    'RFANT',
+    'VNAS',
+    'VNAL',
+    'VNAANT',
+    'VNANON',
+    'VNANOFF',
+    'RFNON',
+    'RFNOFF',
+]
 
 def main():
     """
     Open the serial port, read until a valid JSON line appears or timeout.
     """
-    ms = RFSwitchSerial(sys.argv[-1])
-    ms.start()
-    payload = {"sw_state": 1}
-    try:
-        while True:
-            for i in range(8):
-                sw_state = input("Enter sw_state:")
-                payload["sw_state"] = int(sw_state)
-                ms.command(payload)
-            # payload['sw_state'] = 255
-            # ms.command(payload)
-            # time.sleep(2)
-            # payload['sw_state'] = 0
-            # ms.command(payload)
-            # time.sleep(2)
-    finally:
-        ms.stop()
+    rfsw = picohost.PicoRFSwitch(sys.argv[-1])
+    rfsw.connect()  #XXX why??
+    for state in STATES:
+        print(f"RF switch state: {state}")
+        rfsw.switch(state)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
