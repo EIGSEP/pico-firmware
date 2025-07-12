e663f9ebdcda91762084944dfb22e75172985d44
Charlie Tolley <chbye@berkeley.edu>
2025-07-11 19:07:18 -0700

    Fixed bug in NULL pointer check on JSON in imu and rfswitch code. 2nd imu now works

--- src/imu.cpp
+++ src/imu.cpp
@@ -94,7 +94,7 @@ void calibrate_imu(EigsepImu *eimu) {
 // calibrate if user sends {calibrate: true} in JSON
 void imu_server(uint8_t app_id, const char *json_str) {
     cJSON *root = cJSON_Parse(json_str);
-    if (!root == NULL) return;
+    if (root == NULL) return;
     cJSON *cal_json = cJSON_GetObjectItem(root, "calibrate");
     if (cal_json && cJSON_IsTrue(cal_json)) {
         imu.do_calibration = true;
