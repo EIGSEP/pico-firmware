14103146f1c52b8be58a6c5569298807f1829b73
Charlie Tolley <chbye@berkeley.edu>
2025-07-14 09:38:56 -0700

    import PicoIMU

--- picohost/src/picohost/__init__.py
+++ picohost/src/picohost/__init__.py
@@ -6,4 +6,4 @@ from .base import PicoDevice, PicoRFSwitch, PicoPeltier
 from .motor import PicoMotor
 from . import testing
 
-__all__ = ["PicoDevice", "PicoMotor", "PicoRFSwitch", "PicoPeltier"]
+__all__ = ["PicoDevice", "PicoMotor", "PicoRFSwitch", "PicoPeltier", "PicoIMU"]
