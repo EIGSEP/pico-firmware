a503426a4f18c60c06d365f5ebfb9e43ee3505d0
planetaryCat <richardjs@berkeley.edu>
2025-11-10 11:51:43 -0800

    applied clock divider scaling for PWM output frequency.

--- src/tempctrl.c
+++ src/tempctrl.c
@@ -53,6 +53,7 @@ void init_single_tempctrl(TempControl *tempctrl,
 
 void tempctrl_init(uint8_t app_id) {
     pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 50.0f);         // PWM frequency = System_Clock / (Clock_Divider × (WRAP + 1))
     pwm_config_set_wrap(&config, PWM_WRAP);
     init_single_tempctrl(&tempctrlA, PELTIER1_DIR_PIN1, PELTIER1_DIR_PIN2,
             PELTIER1_PWM_PIN, &config, TEMP_SENSOR1_PIN, pio0);
