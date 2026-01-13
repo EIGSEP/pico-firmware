a4ce71a37cca0b178453037c8c8b572156ba507f
Christian Hellum Bye <chbye@berkeley.edu>
2026-01-13 11:24:24 -0800

    bump ver number, add picostatus to __all__

--- picohost/src/picohost/__init__.py
+++ picohost/src/picohost/__init__.py
@@ -6,4 +6,5 @@ from .base import PicoDevice, PicoRFSwitch, PicoPeltier, PicoIMU
 from .motor import PicoMotor
 from . import testing
 
-__all__ = ["PicoDevice", "PicoMotor", "PicoRFSwitch", "PicoPeltier", "PicoIMU"]
+__all__ = ["PicoDevice", "PicoMotor", "PicoRFSwitch", "PicoStatus", "PicoPeltier", "PicoIMU"]
+__version__ = "0.0.3"
